0ec61cd78e1f5ae7a28a092717d068b046934da6
swamini05 <91067902+swamini05@users.noreply.github.com>
2023-11-23 00:42:03 +0530

    orderidpage

--- src/components/OrderDetails.jsx
+++ src/components/OrderDetails.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { ChevronLeftIcon } from '@heroicons/react/solid';
 
 const OrderDetails = () => {
   // Access the orderId from the URL params using useParams hook
   const { orderId } = useParams();
   console.log(orderId);
-  const history = useHistory();
 
   const handleBack = () => {
     // Go back to the previous page when the "Back" button is clicked
@@ -31,7 +30,7 @@ const OrderDetails = () => {
   };
 
   return (
-    <div className="flex flex-col lg:flex-row justify-center lg:justify-between gap-6 lg:gap-12 p-4 lg:p-8">
+    <div className="flex flex-col lg:flex-row justify-center bg-white rounded m-4 lg:justify-between gap-6 lg:gap-12 p-4 lg:p-8">
       {/* Left Side */}
       <div className="w-full lg:w-1/2">
       <div className="flex items-center mb-4">
@@ -44,33 +43,35 @@ const OrderDetails = () => {
           <h2 className="text-2xl font-bold">Order Details - {orderId}</h2>
         </div>
         <div className="space-y-2">
-          <div className="flex border-b-2 border-gray-300 py-2">
-            <h3 className="font-semibold w-1/3 text-lg">Name:</h3>
-            <p className="w-2/3">{orderDetails.name}</p>
+          <div className="space-y-2 border-2 border-black rounded">
+          <div className="flex border-b-2 border-black py-2">
+            <h3 className="ml-2 font-semibold w-1/3 text-lg">Name:</h3>
+            <p className="w-2/3 text-lg">{orderDetails.name}</p>
           </div>
-          <div className="flex border-b-2 border-gray-300 py-2">
-            <h3 className="font-semibold w-1/3 text-lg">Contact:</h3>
-            <p className="w-2/3">{orderDetails.contact}</p>
+          <div className="flex border-b-2 border-black py-2">
+            <h3 className="ml-2 font-semibold w-1/3 text-lg">Contact:</h3>
+            <p className="w-2/3 text-lg">{orderDetails.contact}</p>
           </div>
-          <div className="flex border-b-2 border-gray-300 py-2">
-            <h3 className="font-semibold w-1/3 text-lg">Location:</h3>
-            <p className="w-2/3">{orderDetails.location}</p>
+          <div className="flex border-b-2 border-black py-2">
+            <h3 className="ml-2 font-semibold w-1/3 text-lg">Location:</h3>
+            <p className="w-2/3 text-lg">{orderDetails.location}</p>
           </div>
-          <div className="flex border-b-2 border-gray-300 py-2">
-            <h3 className="font-semibold w-1/3 text-lg">Brand:</h3>
-            <p className="w-2/3">{orderDetails.brand}</p>
+          <div className="flex border-b-2 border-black py-2">
+            <h3 className="ml-2 font-semibold w-1/3 text-lg">Brand:</h3>
+            <p className="w-2/3 text-lg">{orderDetails.brand}</p>
           </div>
-          <div className="flex border-b-2 border-gray-300 py-2">
-            <h3 className="font-semibold w-1/3 text-lg">Model:</h3>
-            <p className="w-2/3">{orderDetails.model}</p>
+          <div className="flex border-b-2 border-black py-2">
+            <h3 className="ml-2 font-semibold w-1/3 text-lg">Model:</h3>
+            <p className="w-2/3 text-lg">{orderDetails.model}</p>
           </div>
-          <div className="flex border-b-2 border-gray-300 py-2">
-            <h3 className="font-semibold w-1/3 text-lg">Fuel Type:</h3>
-            <p className="w-2/3">{orderDetails.fuelType}</p>
+          <div className="flex border-b-2 border-black py-2">
+            <h3 className="ml-2 font-semibold w-1/3 text-lg">Fuel Type:</h3>
+            <p className="w-2/3 text-lg">{orderDetails.fuelType}</p>
           </div>
-          <div className="flex border-b-2 border-gray-300 py-2">
-            <h3 className="font-semibold w-1/3 text-lg">Status:</h3>
-            <p className="w-2/3">{orderDetails.status}</p>
+          <div className="flex py-2">
+            <h3 className="ml-2 font-semibold w-1/3 text-lg">Status:</h3>
+            <p className="w-2/3 text-lg">{orderDetails.status}</p>
+          </div>
           </div>
           <div className="flex justify-center">
             <button
