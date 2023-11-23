03071fcf2630e54db97b91be5870fe555a5ac121
Swamini <91067902+swamini05@users.noreply.github.com>
2023-11-23 14:25:50 +0530

    Orderdetailslayout

--- src/components/OrderDetails.jsx
+++ src/components/OrderDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { ChevronLeftIcon } from '@heroicons/react/solid';
 
@@ -9,10 +9,10 @@ const OrderDetails = () => {
 
   const handleBack = () => {
     // Go back to the previous page when the "Back" button is clicked
-    history.goBack();
   };
 
-  // Sample data for demonstration
+  const [isClosed, setIsClosed] = useState();
+
   const orderDetails = {
     name: 'John Doe',
     contact: 'john@example.com',
@@ -20,9 +20,17 @@ const OrderDetails = () => {
     brand: 'Brand X',
     model: 'Model Y',
     fuelType: 'Petrol',
-    status: 'Pending',
+    status: 'closed',
     servicesRequested: ['Service A'],
   };
+
+  useEffect(() => {
+    if (orderDetails.status === 'closed'){
+      setIsClosed(true);
+    }else{
+      setIsClosed(false);
+    }
+  })
 
   // Function to handle assigning a service provider
   const assignProvider = () => {
@@ -68,26 +76,42 @@ const OrderDetails = () => {
             <h3 className="ml-2 font-semibold w-1/3 text-lg">Fuel Type:</h3>
             <p className="w-2/3 text-lg">{orderDetails.fuelType}</p>
           </div>
-          <div className="flex py-2">
+          <div className="flex border-b-2 border-black py-2">
             <h3 className="ml-2 font-semibold w-1/3 text-lg">Status:</h3>
             <p className="w-2/3 text-lg">{orderDetails.status}</p>
           </div>
+          {isClosed && (
+            <>
+          <div className="flex border-b-2 border-black py-2">
+            <h3 className="ml-2 font-semibold w-1/3 text-lg">Amount:</h3>
+            <p className="w-2/3 text-lg"></p>
           </div>
-          <div className="flex justify-center">
-            <button
-              className="bg-green-500 text-white px-4 py-2 rounded-md mr-4"
-              // style={{ marginRight: '50px', marginLeft: '150px' }}
-              onClick={assignProvider}
-            >
-              Accept
-            </button>
-            <button
-              className="bg-red-500 text-white px-4 py-2 rounded-md ml-4"
-              onClick={assignProvider}
-            >
-              Reject
-            </button>
+          <div className="flex border-b-2 border-black py-2">
+          <h3 className="ml-2 font-semibold w-1/3 text-lg">Payment Type:</h3>
+          <p className="w-2/3 text-lg"></p>
+        </div><div className="flex border-b-2 border-black py-2">
+            <h3 className="ml-2 font-semibold w-1/3 text-lg">Payment Status:</h3>
+            <p className="w-2/3 text-lg"></p>
           </div>
+          </>
+          )}
+          </div>
+          {!isClosed && (
+  <div className="flex justify-center">
+    <button
+      className="bg-emerald-300 px-4 py-2 rounded-md mr-4 hover:bg-emerald-400 transition-colors duration-300"
+      onClick={assignProvider}
+    >
+      Accept
+    </button>
+    <button
+      className="bg-gray-200 px-4 py-2 rounded-md ml-4 hover:bg-red-500 transition-colors duration-300"
+      onClick={assignProvider}
+    >
+      Reject
+    </button>
+  </div>
+)}
         </div>
       </div>
 
@@ -95,39 +119,56 @@ const OrderDetails = () => {
       <div className="w-full lg:w-1/2">
       <h2 className="text-2xl font-bold mb-12"></h2>
         <div className="space-y-2">
-          <div className="flex border-b-2 border-gray-300 py-2">
-            <h3 className="font-semibold w-1/3 text-lg">Services Requested:</h3>
+        <div className="space-y-2 border-2 border-black rounded">
+          <div className="flex border-b-2 border-black py-2">
+            <h3 className="ml-2 font-semibold w-1/3 text-lg">Services Requested:</h3>
             <div className="w-2/3">
               {orderDetails.servicesRequested.map((service, index) => (
                 <p key={index}>{service}</p>
               ))}
             </div>
           </div>
-          <div className="flex border-b-2 border-gray-300 py-2">
-            <h3 className="font-semibold w-1/3 lg:w-2/5 text-lg">Additional Detail:</h3>
-            <textarea className="w-full lg:w-3/5 h-30 border border-gray-300 rounded-md p-2"></textarea>
+          <div className="flex border-b-2 border-black py-2">
+            <h3 className="ml-2 font-semibold w-1/5 lg:w-2/5 text-lg">Additional Detail:</h3>
+            <textarea className="w-full lg:w-2/3 border border-black rounded-md p-2 mr-2"></textarea>
           </div>
-          <div className="flex border-b-2 border-gray-300 py-2">
-            <h3 className="font-semibold w-1/3 text-lg">Service Type:</h3>
+          <div className="flex border-b-2 border-black py-2">
+            <h3 className="ml-2 font-semibold w-1/3 text-lg">Service Type:</h3>
             <p className="w-2/3">{/* Add service type details */}</p>
           </div>
-          <div className="flex border-b-2 border-gray-300 py-2">
-            <h3 className="font-semibold w-1/3 text-lg">Service Provider:</h3>
+          <div className="flex border-b-2 border-black py-2">
+            <h3 className="ml-2 font-semibold w-1/3 text-lg">Service Provider:</h3>
             <p className="w-2/3">{/* Add service provider details */}</p>
           </div>
-          <div className="flex border-b-2 border-gray-300 py-2">
-            <h3 className="font-semibold w-1/3 text-lg">Assigned:</h3>
-            <p className="w-2/3">{/* Add assigned details */}</p>
+          {!isClosed && (
+          <div className="flex border-b-2 border-black py-2">
+            <h3 className="ml-2 font-semibold w-1/3 text-lg">Assigned:</h3>
+            <p className="w-2/3 mb-24">{/* Add assigned details */}</p>
           </div>
+          )}
+          {isClosed && (
+            <>
+              <div className="flex border-b-2 border-black py-2">
+                <h3 className="ml-2 font-semibold w-1/3 text-lg">Rating:</h3>
+                <p className="w-2/3">{/* Add assigned details */}</p>
+              </div>
+              <div className="flex border-b-2 border-black py-2">
+                <h3 className="ml-2 font-semibold w-1/5 lg:w-2/5 text-lg">Comments:</h3>
+                <textarea className="w-full lg:w-2/3 border border-black rounded-md p-2 mr-2"></textarea>
+              </div>
+            </>
+          )}
+          </div>
+          {!isClosed && (
           <div className='flex justify-center'>
             <button
-              className="bg-blue-500 text-white px-4 py-2 rounded-md"
+              className="bg-emerald-300 px-4 py-2 rounded-md hover:bg-emerald-400 transition-colors duration-300"
               onClick={assignProvider}
-              style={{ marginTop: '4.3rem' }}
               >
               Assign Provider
             </button>
           </div>
+          )}
         </div>
       </div>
     </div>
